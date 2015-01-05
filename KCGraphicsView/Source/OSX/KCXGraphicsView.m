98998618a33e9f92e0783a256d6149390040cefc
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-01-06 00:06:03 +0900

    test: Update unit-test for OSX

--- KCGraphicsView/Source/OSX/KCXGraphicsView.m
+++ KCGraphicsView/Source/OSX/KCXGraphicsView.m
@@ -9,10 +9,21 @@
 
 @implementation KCGraphicsView
 
-- (void)drawRect:(NSRect)dirtyRect {
-    [super drawRect:dirtyRect];
+- (void) drawRect:(NSRect) dirtyRect
+{
+	[super drawRect:dirtyRect];
     
-    // Drawing code here.
+	// Drawing code here.
+	CGContextRef context = [[NSGraphicsContext currentContext] graphicsPort] ;
+	CGContextSetRGBFillColor(context, 1, 1, 1, 1);//塗りつぶし色
+	CGContextFillRect(context, NSRectToCGRect(dirtyRect));// 四角形を塗りつぶす
+	CGContextSaveGState(context);// グラフィック状態の保存
+	CGContextSetRGBStrokeColor(context,0,0,1,1);// 線色
+	CGContextSetLineWidth(context, 10.5); // 線の太さを変更
+	CGContextMoveToPoint(context,10,200);
+	CGContextAddLineToPoint(context, 200,200);
+	CGContextStrokePath(context); // 描画！
+	CGContextRestoreGState(context);// さっき保存した状態に戻す
 }
 
 @end
