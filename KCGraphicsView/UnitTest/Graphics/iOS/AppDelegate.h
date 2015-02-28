42175c06c7e6eabb5e71a1d24995c226fd3cb9bb
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-02-28 21:04:47 +0900

    test: Add unit test "Graphics" for iOS

---
+++ KCGraphicsView/UnitTest/Graphics/iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Graphics
+//
+//  Created by Tomoo Hamada on 2015/02/28.
+//  Copyright (c) 2015年 Steel Wheels Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KCGraphicsView/UnitTest/Graphics/iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Graphics
+//
+//  Created by Tomoo Hamada on 2015/02/28.
+//  Copyright (c) 2015年 Steel Wheels Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
