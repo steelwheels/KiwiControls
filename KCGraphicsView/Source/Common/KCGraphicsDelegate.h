1f0045c30f5312b6a4885edcae85728593dde011
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-03-11 23:21:35 +0900

    KCGraphicsView: Add delegate to KCGraphicsView

---
+++ KCGraphicsView/Source/Common/KCGraphicsDelegate.h
@@ -0,0 +1,10 @@
+/**
+ * @file	KCGraphicsDelegate.h
+ * @brief	Define KCGraphicsDelegate delegate
+ * @par Copyright
+ *   Copyright (C) 2014 Steel Wheels Project
+ */
+
+@protocol KCGraphicsDelegate <NSObject>
+- (void) editingGraphicsDone ;
+@end
