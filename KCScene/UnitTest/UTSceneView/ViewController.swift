09c3c11d88bfa2e6de43bf926fa2a33f6b5aa0af
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-04-30 09:19:58 +0900

    KSScene: Add objects at the unit test

--- KCScene/UnitTest/UTSceneView/ViewController.swift
+++ KCScene/UnitTest/UTSceneView/ViewController.swift
@@ -28,42 +28,33 @@ class ViewController: NSViewController, SCNSceneRendererDelegate
 		// Do any additional setup after loading the view.
 		sceneView.backgroundColor = NSColor.blueColor()
 
-/*
 		let floor0 = SCNNode()
 		floor0.geometry = SCNFloor()
 		floor0.position = SCNVector3(x: 0.0, y:0.0, z:0.0)
 		floor0.geometry?.firstMaterial?.diffuse.contents = NSColor.whiteColor()
 		sceneView.addChildNode(floor0)
-*/
 
 		let sphere0 = SCNNode()
 		sphere0.geometry = SCNSphere(radius: 50.0)
 		sphere0.position = SCNVector3(0.0, 0.0, 0.0)
 		sceneView.addChildNode(sphere0)
 
-/*
 		let box0 = SCNNode()
-		box0.geometry = SCNBox(width:20, height:10, length:100, chamferRadius:0.02)
-		box0.position = SCNVector3(x: 20, y:0.0, z:0.0)
+		box0.geometry = SCNBox(width:20, height:40, length:20, chamferRadius:0.02)
+		box0.position = SCNVector3(x:  80, y:0.0, z:0.0)
 		sceneView.addChildNode(box0)
 
 		let box1 = SCNNode()
-		box1.geometry = SCNBox(width:20, height:10, length:100, chamferRadius:0.02)
-		box1.position = SCNVector3(x: -20, y:0.0, z:0.0)
+		box1.geometry = SCNBox(width:20, height:40, length:20, chamferRadius:0.02)
+		box1.position = SCNVector3(x: -80, y:0.0, z:0.0)
 		sceneView.addChildNode(box1)
-
+/*
 		let cone2 = SCNNode()
 		cone2.geometry = SCNCone(topRadius: 2.0, bottomRadius: 10.0, height: 50.0)
 		cone2.position = SCNVector3(x: -30, y:0.0, z:0.0)
 		cone2.lookAt(SCNVector3(0.0, 0.0, 0.0))
 		sceneView.addChildNode(cone2)
 */
-		//let camera = sceneView.cameraNode
-		//let light  = sceneView.lightNode
-		//camera.position	= SCNVector3(x: 0.0, y: 100.0, z: 100.0)
-		//light.position	= SCNVector3(x: 0.0, y: 100.0, z: 100.0)
-		//camera.lookAt(SCNVector3(0.0, 0.0, 0.0))
-
 		Swift.print("[Camera]")
 		sceneView.cameraNode.dumpToConsole(console)
 
