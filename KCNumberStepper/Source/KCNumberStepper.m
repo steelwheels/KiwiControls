83fb16fe4b98496b9705276ecb55af6088c036fa
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-05-07 12:28:16 +0900

    doc: Remove current implementations

--- KCNumberStepper/Source/KCNumberStepper.m
+++
@@ -1,9 +0,0 @@
-/**
- * @file	KCNumberStepper.m
- * @brief	Header file to import all header files for KCNumberStepper
- * @par Copyright
- *   Copyright (C) 2015 Steel Wheels Project
- */
-
-#import "KCNumberStepper.h"
-
--- KCTextFieldExtension/Source/KCTextFieldForwarders.h
+++
@@ -1,9 +0,0 @@
-/**
- * @file	KCTextFieldForderders.h
- * @brief	Forward declaration for KCTextFieldExtension libary
- * @par Copyright
- *   Copyright (C) 2014 Steel Wheels Project
- */
-
-@class KCTextFieldCommand ;
-
