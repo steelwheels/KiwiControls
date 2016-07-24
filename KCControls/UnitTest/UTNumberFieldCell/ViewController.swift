313681b298924cdbfe09d74187e21811fa86a7e5
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-07-24 12:31:49 +0900

    KCControls: Add KCNumberField class and unit test for it

---
+++ KCControls/UnitTest/UTNumberFieldCell/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UTNumberFieldCell
+//
+//  Created by Tomoo Hamada on 2016/07/24.
+//  Copyright © 2016年 Steel Wheels Project. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
