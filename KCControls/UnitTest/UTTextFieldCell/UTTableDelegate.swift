6bf5b7181bdcc96dc3c1a81babdbafef1c8f05a8
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-07-27 00:40:46 +0900

    KCControls: Add "UTTextField" unit test

---
+++ KCControls/UnitTest/UTTextFieldCell/UTTableDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  UTTableDelegate.swift
+//  KCControls
+//
+//  Created by Tomoo Hamada on 2016/07/27.
+//  Copyright © 2016年 Steel Wheels Project. All rights reserved.
+//
+
+import Foundation
---
+++ KCControls/UnitTest/UTTextFieldCell/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UTTextFieldCell
+//
+//  Created by Tomoo Hamada on 2016/07/26.
+//  Copyright © 2016年 Steel Wheels Project. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController
+{
+	@IBOutlet weak var mTableView: NSTableView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+}
+
