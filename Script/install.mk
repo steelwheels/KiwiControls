164160d9644b96e429ff8ff87140565260dc9acc
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-09-19 15:29:04 +0900

    build: Update install script

---
+++ Script/install.mk
@@ -0,0 +1,12 @@
+#
+# install.mk
+#
+
+install: dummy
+	xcodebuild build -target $(PROJECT_NAME) -project $(PROJECT_NAME).xcodeproj \
+	  -configuration Release DSTROOT=/ ONLY_ACTIVE_ARCH=NO
+	xcodebuild install -target $(PROJECT_NAME) -project $(PROJECT_NAME).xcodeproj \
+	  -configuration Release DSTROOT=/ ONLY_ACTIVE_ARCH=NO
+
+dummy:
+
