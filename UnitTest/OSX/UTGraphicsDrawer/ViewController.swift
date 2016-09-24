fc8387cd8a27e90eb0bf908cd3b3e08c87e76b7a
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-09-24 10:56:44 +0900

    test: Add unit test: "UTGraphicsDrawer"

---
+++ UnitTest/OSX/UTGraphicsDrawer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UTGraphicsDrawer
+//
+//  Created by Tomoo Hamada on 2016/09/24.
+//  Copyright © 2016年 Steel Wheels Project. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
