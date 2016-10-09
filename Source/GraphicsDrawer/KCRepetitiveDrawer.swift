810c4719426d9411342cfaef692587664e920242
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-10-09 16:51:57 +0900

    test: Add unit test: UTGraphicsLayer

--- Source/GraphicsDrawer/KCRepetitiveDrawer.swift
+++ Source/GraphicsDrawer/KCRepetitiveDrawer.swift
@@ -44,7 +44,6 @@ public class KCRepetitiveDrawer: KCGraphicsLayer
 				ctxt.draw(layer!, at: point)
 			}
 		}
-
 	}
 }
 
---
+++ UnitTest/OSX/UTGraphicsLayer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UTGraphicsLayer
+//
+//  Created by Tomoo Hamada on 2016/10/09.
+//  Copyright © 2016年 Steel Wheels Project. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
