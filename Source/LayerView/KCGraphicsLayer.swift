59822b414bf4695182ba8a4ccc458fb109ba12d0
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-12-14 00:59:07 +0900

    KCGraphicsLayer: Update to exchange drawing method

--- Source/LayerView/KCGraphicsLayer.swift
+++ Source/LayerView/KCGraphicsLayer.swift
@@ -16,15 +16,32 @@ import KiwiGraphics
 
 open class KCGraphicsLayer: KCLayer
 {
-	private var mLayerDrawer	: KGImageDrawer
+	private var mLayerDrawer	: KGImageDrawer?
 
 	public init(frame frm: CGRect, drawer drw: @escaping KGImageDrawer){
 		mLayerDrawer = drw
 		super.init(frame: frm)
-		self.image = KGImage.generate(size: frame.size, drawFunc: mLayerDrawer)
+		self.image = KGImage.generate(size: frame.size, drawFunc: drw)
+	}
+
+	public override init(frame frm: CGRect){
+		mLayerDrawer = nil
+		super.init(frame: frm)
 	}
 
 	public required init?(coder decoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
+	}
+
+	public var layerDrawer: KGImageDrawer? {
+		get {
+			return mLayerDrawer
+		}
+		set(drawer){
+			mLayerDrawer = drawer
+			if let drawer = mLayerDrawer {
+				self.image = KGImage.generate(size: frame.size, drawFunc: drawer)
+			}
+		}
 	}
 }
