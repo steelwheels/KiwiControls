3053a91280f962b276be795978fbbddf167fa906
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2017-04-23 19:12:58 +0900

    KCStackView: Add unit tests for macOS anx iOS

---
+++ UnitTest/OSX/UTStackView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UTStackView
+//
+//  Created by Tomoo Hamada on 2017/04/23.
+//  Copyright © 2017年 Steel Wheels Project. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
