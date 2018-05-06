619be67c54dc9822663a6df3e24b6f13f9905eec
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2018-05-06 10:14:17 +0900

    test: Add unit test: UTLogWindow

---
+++ UnitTest/OSX/UTLogWindow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UTLogWindow
+//
+//  Created by Tomoo Hamada on 2018/05/06.
+//  Copyright © 2018年 Steel Wheels Project. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ UnitTest/OSX/UTLogWindow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UTLogWindow
+//
+//  Created by Tomoo Hamada on 2018/05/06.
+//  Copyright © 2018年 Steel Wheels Project. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
