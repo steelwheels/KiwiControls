fdee7d95d81d49cc6811b65d8fceee3a5cd87147
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2018-09-13 23:47:05 +0900

    CNSizeFitLayouter, CNFrameAllocLayouter: Add classs for layout

---
+++ Source/Layout/KCFrameAllocLayouter.swift
@@ -0,0 +1,9 @@
+//
+//  KCFrameAllocLayouter.swift
+//  KiwiControls
+//
+//  Created by Tomoo Hamada on 2018/09/13.
+//  Copyright © 2018年 Steel Wheels Project. All rights reserved.
+//
+
+import Foundation
---
+++ Source/Layout/KCSizeFitLayouter.swift
@@ -0,0 +1,9 @@
+//
+//  KCSizeFitLayouter.swift
+//  KiwiControls
+//
+//  Created by Tomoo Hamada on 2018/09/12.
+//  Copyright © 2018年 Steel Wheels Project. All rights reserved.
+//
+
+import Foundation
