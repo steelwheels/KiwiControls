1f90e60c5472cc7dc49524b9b098f6884df53699
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2018-12-26 00:58:46 +0900

    KCSpriteView: Change file location, Update unit test for sprite-view for iOS

--- UnitTest/iOS/UTSpriteView/ViewController.swift
+++ UnitTest/iOS/UTSpriteView/ViewController.swift
@@ -19,45 +19,41 @@ class ViewController: UIViewController
 		// Do any additional setup after loading the view.
 		mSpriteView.backgroundColorOfScene = .yellow
 
-		let b0init = KCSpriteViewDatabase.makeParameter(imageFile: "blue-machine",
-								scale: 0.5,
-								alpha: 1.0,
-								position: CGPoint(x:10.0, y:10.0),
-								rotation: 0.0,
-								duration: 1.0)
-		if !mSpriteView.database.create(identifier: "b0", value: b0init) {
-			NSLog("Failed to create (b0)")
-		}
+		let b0init = KCSpriteNode(imageFile: "blue-machine",
+					  scale: 0.5,
+					  alpha: 1.0,
+					  position: CGPoint(x: 10.0, y: 10.0),
+					  rotation: 0.0,
+					  duration: 1.0)
+		mSpriteView.database.write(identifier: "b0", value: b0init.toValue())
 
-		let g0init = KCSpriteViewDatabase.makeParameter(imageFile: "green-machine",
-								scale: 0.5,
-								alpha: 1.0,
-								position: CGPoint(x:370.0, y:650.0),
-								rotation: 0.0,
-								duration: 1.0)
-		if !mSpriteView.database.create(identifier: "g0", value: g0init) {
-			NSLog("Failed to create (g0)")
-		}
+		let g0init = KCSpriteNode(imageFile: "green-machine",
+					  scale: 0.5,
+					  alpha: 1.0,
+					  position: CGPoint(x: 370.0, y: 650.0),
+					  rotation: 0.0,
+					  duration: 1.0)
+		mSpriteView.database.write(identifier: "g0", value: g0init.toValue())
 
-		let b0param = KCSpriteViewDatabase.makeParameter(imageFile: "blue-machine",
-								 scale: 0.5,
-								 alpha: 1.0,
-								 position: CGPoint(x:190.0, y:320.0),
-								 rotation: Double.pi * 2.0 / 4.0,
-								 duration: 1.0)
-		if !mSpriteView.database.write(identifier: "b0", value: b0param) {
-			NSLog("Failed to write (b0)")
-		}
+		mSpriteView.database.commit()
 
-		let g0param = KCSpriteViewDatabase.makeParameter(imageFile: "green-machine",
-								 scale: 0.5,
-								 alpha: 1.0,
-								 position: CGPoint(x:210.0, y:340.0),
-								 rotation: -Double.pi * 2.0 / 4.0,
-								 duration: 1.0)
-		if !mSpriteView.database.write(identifier: "g0", value: g0param) {
-			NSLog("Failed to write (g0)")
-		}
+		let b0param = KCSpriteNode(imageFile: "blue-machine",
+					  scale: 0.5,
+					  alpha: 1.0,
+					  position: CGPoint(x: 190.0, y: 320.0),
+					  rotation: 0.0,
+					  duration: 1.0)
+		mSpriteView.database.write(identifier: "b0", value: b0param.toValue())
+
+		let g0param = KCSpriteNode(imageFile: "green-machine",
+					  scale: 0.5,
+					  alpha: 1.0,
+					  position: CGPoint(x: 170.0, y: 340.0),
+					  rotation: 0.0,
+					  duration: 1.0)
+		mSpriteView.database.write(identifier: "g0", value: g0param.toValue())
+
+		mSpriteView.database.commit()
 
 		/* Update the database */
 		mSpriteView.database.commit()
