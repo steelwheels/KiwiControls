9663e51c986144efc0ef96e942b4d776e360a979
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2020-06-13 16:25:29 +0900

    KCContextualMenu: Initial commit
    The unit test UTStackView is updated to test it.

---
+++ Source/Controls/KCContextualMenu.swift
@@ -0,0 +1,55 @@
+/**
+ * @file KCContextualMenu.swift
+ * @brief Define KCContextualMenu class
+ * @par Copyright
+ *   Copyright (C) 2020 Steel Wheels Project
+ */
+
+import Cocoa
+import Foundation
+
+public class KCContextualMenu
+{
+	public struct MenuItem {
+		var index:	Int
+		var label:	String
+
+		public init(index idx: Int, label lab: String){
+			index = idx
+			label = lab
+		}
+	}
+
+	private var mContextualMenu:	NSMenu
+	private var mSelectedItem:	Int?
+
+	public init() {
+		mContextualMenu = NSMenu(title: "Contextual menu")
+		mSelectedItem	= nil
+	}
+
+	public func add(menuItems items: Array<MenuItem>) {
+		let sel = #selector(self.menuAction)
+		for item in items {
+			let newitem = NSMenuItem(title: item.label, action: sel, keyEquivalent: "")
+			newitem.target    = self
+			newitem.isEnabled = true
+			newitem.tag       = item.index
+			mContextualMenu.addItem(newitem)
+		}
+	}
+
+	@objc private func menuAction(_ item : NSMenuItem) {
+		mSelectedItem = item.tag
+	}
+
+	public func show(at position: KCPoint, in view: KCView) -> Int? {
+		mSelectedItem = nil
+		if mContextualMenu.popUp(positioning: mContextualMenu.item(at: 0), at: position, in: view) {
+			return mSelectedItem
+		} else {
+			return nil
+		}
+	}
+}
+
--- UnitTest/OSX/UTStackView/ViewController.swift
+++ UnitTest/OSX/UTStackView/ViewController.swift
@@ -13,8 +13,15 @@ class ViewController:  KCPlaneViewController
 	private var mStackView:		KCStackView? = nil
 
 	open override func loadViewContext(rootView root: KCRootView) -> KCSize {
+		let buttonview = KCButton()
+		buttonview.title = "Hello"
+		buttonview.buttonPressedCallback = {
+			() -> Void in
+			self.popupMenu()
+		}
 		let stackview = KCStackView()
 		root.setup(childView: stackview)
+		stackview.addArrangedSubView(subView: buttonview)
 		mStackView = stackview
 		return stackview.fittingSize
 	}
@@ -32,6 +39,21 @@ class ViewController:  KCPlaneViewController
 	override var representedObject: Any? {
 		didSet {
 		// Update the view, if already loaded.
+		}
+	}
+
+	private func popupMenu() {
+		if let parent = mStackView {
+			let item0 = KCContextualMenu.MenuItem(index: 0, label: "label0")
+			let item1 = KCContextualMenu.MenuItem(index: 0, label: "label1")
+			let menu  = KCContextualMenu()
+			let pos   = KCPoint(x: 0.0, y: 0.0)
+			menu.add(menuItems: [item0, item1])
+			if let idx = menu.show(at: pos, in: parent) {
+				NSLog("popupmenu - selected: \(idx)")
+			} else {
+				NSLog("popupmenu - not selected")
+			}
 		}
 	}
 }
