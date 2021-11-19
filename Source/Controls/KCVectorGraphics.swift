42af08013e9eee1a26404668a9ed3a08b64aa2ba
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2021-11-20 08:49:40 +0900

    KCVectorGraphics: Update to use CNBezierPath class

--- Source/Controls/KCVectorGraphics.swift
+++ Source/Controls/KCVectorGraphics.swift
@@ -14,6 +14,90 @@
 #endif
 import CoconutData
 
+public extension CNVectorObject
+{
+	func draw(bezierPath path: CNBezierPath){
+		if self.doFill {
+			path.fill()
+		} else {
+			path.stroke()
+		}
+	}
+}
+
+public extension CNVectorPath
+{
+	func draw(in area: CGSize) {
+		let points = self.normalize(in: area)
+		if points.count >= 2 {
+			let bezier = allocateBezierPath()
+			bezier.move(to: points[0])
+			for i in 1..<points.count {
+				bezier.addLine(to: points[i])
+			}
+			super.draw(bezierPath: bezier)
+		}
+	}
+}
+
+public extension CNVectorRect
+{
+	func draw(in area: CGSize) {
+		if let normrect = self.normalize(in: area) {
+			let bezier = allocateBezierPath()
+			if self.isRounded {
+				bezier.appendRoundedRect(normrect, xRadius: 10.0, yRadius: 10.0)
+			} else {
+				bezier.appendRect(normrect)
+			}
+			super.draw(bezierPath: bezier)
+		}
+	}
+}
+
+public extension CNVectorOval
+{
+	func draw(in area: CGSize) {
+		if let (center, radius) = self.normalize(in: area) {
+			let bezier = allocateBezierPath()
+			#if os(OSX)
+				let bounds = CGRect(x: center.x - radius, y: center.y - radius, width: radius * 2.0, height: radius * 2.0)
+				bezier.appendOval(in: bounds)
+			#else
+				bezier.appendOval(center: center, radius: radius)
+			#endif
+			super.draw(bezierPath: bezier)
+		}
+	}
+}
+
+public extension CNVectorString
+{
+	func draw(textField textfield: KCTextEdit, isEdtiable isedit: Bool, in area: CGSize) {
+		if let orgpt = self.normalize(in: area) {
+			if isedit {
+				/* Show text edit */
+				textfield.font = self.font
+				CNVectorString.updateTextFieldLocation(textField: textfield, offset: orgpt)
+				textfield.isHidden = false
+				textfield.requireLayout()
+			} else {
+				textfield.isHidden = true
+				let astr = self.attributedString()
+				astr.draw(at: orgpt)
+			}
+		}
+	}
+
+	static func updateTextFieldLocation(textField field: KCTextEdit, offset ofst: CGPoint){
+		let size   = field.intrinsicContentSize
+		let frame  = CGRect(origin: ofst, size: size)
+		let bounds = CGRect(origin: CGPoint.zero, size: size)
+		field.frame  = frame
+		field.bounds = bounds
+	}
+}
+
 open class KCVectorGraphics: KCView
 {
 	private var mGenerator:		CNVecroGraphicsGenerator
@@ -51,15 +135,7 @@ open class KCVectorGraphics: KCView
 			(_ str: String) -> Void in
 			self.mGenerator.storeString(string: str)
 		}
-		updateTextFieldLocation(textField: mTextField, offset: CGPoint.zero)
-	}
-
-	private func updateTextFieldLocation(textField field: KCTextEdit, offset ofst: CGPoint){
-		let size   = field.intrinsicContentSize
-		let frame  = CGRect(origin: ofst, size: size)
-		let bounds = CGRect(origin: CGPoint.zero, size: size)
-		field.frame  = frame
-		field.bounds = bounds
+		CNVectorString.updateTextFieldLocation(textField: mTextField, offset: CGPoint.zero)
 	}
 
 	public var currentType: CNVectorGraphicsType {
@@ -114,103 +190,18 @@ open class KCVectorGraphics: KCView
 			let gr = contents[i]
 			switch gr {
 			case .path(let path):
-				drawPath(vectorPath: path)
+				path.draw(in: self.frame.size)
 			case .rect(let rect):
-				drawRect(vectorRect: rect)
+				rect.draw(in: self.frame.size)
 			case .oval(let oval):
-				drawOval(vectorOval: oval)
+				oval.draw(in: self.frame.size)
 			case .string(let str):
-				drawString(vectorString: str, isLast: (i == count-1))
+				str.draw(textField: mTextField, isEdtiable: i == count - 1, in: self.frame.size)
 			@unknown default:
 				CNLog(logLevel: .error, message: "Unknown case", atFunction: #function, inFile: #file)
 			}
 		}
 		super.draw(dirtyRect)
-	}
-
-	private func drawPath(vectorPath path: CNVectorPath){
-		let points = path.normalize(in: self.frame.size)
-		if points.count >= 2 {
-			let bezier = allocateBezierPath()
-			let dofill = path.doFill
-			bezier.lineWidth = path.lineWidth
-			setPathColor(vectorObject: path, doFill: dofill)
-
-			bezier.move(to: points[0])
-			for i in 1..<points.count {
-				bezier.addLine(to: points[i])
-			}
-
-			drawBezierPath(bezierPath: bezier, doFill: dofill)
-		}
-	}
-
-	private func drawRect(vectorRect rect: CNVectorRect){
-		if let normrect = rect.normalize(in: self.frame.size) {
-			let bezier = allocateBezierPath()
-			let dofill = rect.doFill
-			bezier.lineWidth = rect.lineWidth
-			if rect.isRounded {
-				bezier.appendRoundedRect(normrect, xRadius: 10.0, yRadius: 10.0)
-			} else {
-				bezier.appendRect(normrect)
-			}
-			setPathColor(vectorObject: rect, doFill: dofill)
-			drawBezierPath(bezierPath: bezier, doFill: dofill)
-		}
-	}
-
-	private func drawOval(vectorOval oval: CNVectorOval){
-		if let (center, radius) = oval.normalize(in: self.frame.size) {
-			let bezier = allocateBezierPath()
-			let dofill = oval.doFill
-			bezier.lineWidth = oval.lineWidth
-			#if os(OSX)
-				let bounds = CGRect(x: center.x - radius, y: center.y - radius, width: radius * 2.0, height: radius * 2.0)
-				bezier.appendOval(in: bounds)
-			#else
-				bezier.appendOval(center: center, radius: radius)
-			#endif
-			setPathColor(vectorObject: oval, doFill: dofill)
-			drawBezierPath(bezierPath: bezier, doFill: dofill)
-		}
-	}
-
-	private func drawString(vectorString str: CNVectorString, isLast islast: Bool){
-		if let orgpt = str.normalize(in: self.frame.size) {
-			if islast {
-				/* Show text edit */
-				mTextField.font = str.font
-				updateTextFieldLocation(textField: mTextField, offset: orgpt)
-				mTextField.isHidden = false
-				mTextField.requireLayout()
-			} else {
-				mTextField.isHidden = true
-				/* Draw text */
-				let attrs: [NSAttributedString.Key: Any] = [
-					NSAttributedString.Key.foregroundColor: str.strokeColor,
-					NSAttributedString.Key.backgroundColor:	CNColor.clear,
-					NSAttributedString.Key.font:		str.font
-				]
-				let astr = NSAttributedString(string: str.string, attributes: attrs)
-				astr.draw(at: orgpt)
-			}
-		}
-	}
-
-	private func allocateBezierPath() -> KCBezierPath {
-		let bezier = KCBezierPath()
-		bezier.lineJoinStyle = .round
-		bezier.lineCapStyle  = .round
-		return bezier
-	}
-
-	private func drawBezierPath(bezierPath path: KCBezierPath, doFill fill: Bool){
-		if fill {
-			path.fill()
-		} else {
-			path.stroke()
-		}
 	}
 
 	private func setPathColor(vectorObject vobj: CNVectorObject, doFill fill: Bool) {
